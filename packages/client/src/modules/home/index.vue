1a80d8276d578c478165bb2faf7f63fa0f71ff9d
ssssszYYDS <xiakj@shanghaitech.edu.cn>
2024-05-12 04:31:55 +0800

    update-timeline-and-scatter-plot

--- packages/client/src/modules/home/index.vue
+++ packages/client/src/modules/home/index.vue
@@ -9,9 +9,7 @@
 
 		</div>
 		<div class="timeline">
-			<button @click="toDetail" style="width: 100%; height: 100%;">
-				Timeline
-			</button>
+			<div class="time-point" @mousedown="startDrag" @mouseup="stopDrag"></div>
 		</div>
 		<div class="under-timeline-plot">
 			<button @click="toDetail" style="width: 100%; height: 100%;">
@@ -52,11 +50,19 @@ export default {
 	data() {
 		return {
 			svg: null, // 保存SVG引用以便重新绘制
+			buildings: null, // 保存建筑物数据
+			transports: null, // 保存交通数据
+			xScale: null,
+			yScale: null,
+			speedColorScale: null,
+			timeScale: null,
 		};
 	},
 
 	mounted() {
 		this.initBuildings();
+		this.initTransports('2023-03-03');
+
 		window.addEventListener('resize', this.redrawBuildings);
 	},
 
@@ -70,7 +76,7 @@ export default {
 				const res = await HttpHelper.post(Urls.getCSVData, { path: 'CSVData/building.csv' });
 				console.log("buildings data: ");
 				console.log(res);
-				this.data = res
+				this.buildings = res
 				if (res != null) {
 					this.drawBuildings(res);
 				} else {
@@ -115,10 +121,10 @@ export default {
 				.style("background-color", "lightgrey")
 				.style("border", "1px solid black")
 
-			const xScale = d3.scaleLinear()
+			this.xScale = d3.scaleLinear()
 				.domain([min_x, max_x])
 				.range([width * (1 - picture_range) / 2, width - width * (1 - picture_range) / 2]);
-			const yScale = d3.scaleLinear()
+			this.yScale = d3.scaleLinear()
 				.domain([min_y, max_y])
 				.range([0, height * picture_range]);
 
@@ -128,7 +134,7 @@ export default {
 				.append("polygon")
 				.attr("points", d => d.location.split(", ").map(point => {
 					const [x, y] = point.split(" ").map(parseFloat);
-					return xScale(x) + " " + yScale(y);
+					return this.xScale(x) + " " + this.yScale(y);
 				}).join(" "))
 				.attr("fill", d => buildingType2color[d.buildingType])
 				.attr("stroke", "black")
@@ -142,11 +148,144 @@ export default {
 		},
 
 		redrawBuildings() {
-			console.log("redrawing buildings...");
-			if (!this.data) return; // 确保数据存在
-			console.log("drawBuildings...");
-			this.drawBuildings(this.data); // 重新绘制建筑物
-		}
+			if (this.buildings)
+				this.drawBuildings(this.buildings); // 重新绘制建筑物
+		},
+
+		// date format: '2023-03-03', range: '2022-03-01' to '2023-05-24'
+		async initTransports(date) {
+			try {
+				const res = await HttpHelper.post(Urls.getCSVData, { path: 'CSVData/Transports/transports_' + date + '.csv' });
+
+				var parseTime = d3.timeParse('%Y-%m-%d %H:%M:%S');
+				var formatTime = d3.timeFormat('%Y-%m-%d %H:%M:%S');
+				// Parse the time and calculate average speed
+				res.forEach(function (d) {
+					d.timeStamp = parseTime(d.day + ' ' + d.time);
+					d.timeStampFloat = d.timeStamp.getTime();
+				});
+
+				const width = d3.select("#chart").node().getBoundingClientRect().width;
+				this.timeScale = d3.scaleTime()
+					.domain([0, width])
+					.range(d3.extent(res.map(d => d.timeStampFloat)));
+
+				this.speedColorScale = d3.scaleLinear()
+					.domain([250, 350, 450])
+					.range(["red", "orange", "green"]);
+
+				this.transports = res
+				console.log("transports data: ");
+				console.log(this.transports);
+				if (res != null) {
+					this.drawTimeline(res);
+				} else {
+					console.error("Failed to load transports data:", res);
+				}
+			} catch (error) {
+				console.error("Failed to load transports data:", error);
+			}
+		},
+
+		drawTimeline(data) {
+			if (this.timeline) {
+				this.timeline.remove();
+			}
+
+			this.timeline = d3.select(".timeline")
+				.attr("class", "timeline")
+				.style("width", "100%")
+				.style("height", "100%")
+				.style("border", "1px solid black");
+			this.handle = this.timeline.append("div")
+				.attr("class", "handle")
+				.style("position", "relative")
+				.style("left", "0px")
+				.style("top", "0px")
+				.style("width", "10px")
+				.style("height", "100%")
+				.style("background-color", "blue");
+
+			const width = d3.select("#chart").node().getBoundingClientRect().width;
+
+			var dragBehavior = d3.drag()
+				.on("drag", (event) => {
+					var startTime = new Date().getTime();
+					this.clearTransportsScatterPlot();
+
+					var x = event.x;
+					var max_x = this.timeline.node().getBoundingClientRect().width - this.handle.node().getBoundingClientRect().width;
+					if (x < 0) {
+						x = 0;
+					} else if (x > max_x) {
+						x = max_x;
+					}
+					this.handle.style("left", x + "px");
+
+					var curTime = x / max_x * (this.timeScale.range()[1] - this.timeScale.range()[0]) + this.timeScale.range()[0];
+					var selectData = this.selectDataByCurTime(curTime);
+					this.drawTransportsScatterPlot(selectData);
+
+					console.log("drag consuming time: " + (new Date().getTime() - startTime) + "ms");
+				});
+			this.handle.call(dragBehavior);
+		},
+
+
+		selectDataByCurTime(curTime) {
+			var leftMaxTimeStamp = parseFloat(d3.max(this.transports, d => d.timeStampFloat <= curTime ? d.timeStampFloat : -Infinity));
+			var rightMinTimeStamp = parseFloat(d3.min(this.transports, d => d.timeStampFloat >= curTime ? d.timeStampFloat : Infinity));
+			// 边缘情况
+			if (leftMaxTimeStamp == rightMinTimeStamp)
+				if (parseFloat(d3.min(this.transports, d => d.timeStampFloat > curTime ? d.timeStampFloat : Infinity)) == Infinity) {
+					leftMaxTimeStamp = parseFloat(d3.max(this.transports, d => d.timeStampFloat < curTime ? d.timeStampFloat : -Infinity));
+					rightMinTimeStamp = parseFloat(d3.min(this.transports, d => d.timeStampFloat >= curTime ? d.timeStampFloat : Infinity));
+					console.log("case1" + leftMaxTimeStamp + " " + rightMinTimeStamp)
+				} else {
+					leftMaxTimeStamp = parseFloat(d3.max(this.transports, d => d.timeStampFloat <= curTime ? d.timeStampFloat : -Infinity));
+					rightMinTimeStamp = parseFloat(d3.min(this.transports, d => d.timeStampFloat > curTime ? d.timeStampFloat : Infinity));
+				}
+
+			var leftSelectedData = this.transports.filter(d => d.timeStampFloat === leftMaxTimeStamp);
+			var rightSelectedData = this.transports.filter(d => d.timeStampFloat === rightMinTimeStamp);
+			var curRelativeTime = (curTime - leftMaxTimeStamp) / (rightMinTimeStamp - leftMaxTimeStamp);
+			var timeFormat = d3.timeFormat('%Y-%m-%d %H:%M:%S');
+
+			var selectData = leftSelectedData.filter(leftData =>
+				rightSelectedData.some(rightData => rightData.participantId === leftData.participantId)
+			).map(leftData => {
+				var rightData = rightSelectedData.find(d => d.participantId === leftData.participantId);
+				var tempData = {};
+				tempData.participantId = leftData.participantId;
+				tempData.timeStampFloat = curTime;
+				tempData.timeStamp = timeFormat(new Date(curTime));
+				tempData.loc_x = (1 - curRelativeTime) * leftData.loc_x + curRelativeTime * rightData.loc_x;
+				tempData.loc_y = (1 - curRelativeTime) * leftData.loc_y + curRelativeTime * rightData.loc_y;
+				// tempData.speed = Math.sqrt((rightData.loc_x - leftData.loc_x) ** 2 + (rightData.loc_y - leftData.loc_y) ** 2);
+				tempData.speed = Math.abs(rightData.loc_x - leftData.loc_x) + Math.abs(rightData.loc_y - leftData.loc_y);
+				return tempData;
+			});
+			return selectData;
+		},
+
+		drawTransportsScatterPlot(selectData) {
+			// Draw the scatter plot
+			this.svg.selectAll("circle")
+				.data(selectData)
+				.enter()
+				.append("circle")
+				.attr("class", "scatter-point")
+				.attr("cx", (d) => { return this.xScale(d.loc_x); })
+				.attr("cy", (d) => { return this.yScale(d.loc_y); })
+				.attr("r", 2)
+				.attr("fill-opacity", 1.0)
+				.attr("fill", (d) => { return this.speedColorScale(d.speed); })
+		},
+
+		clearTransportsScatterPlot() {
+			this.svg.selectAll("circle.scatter-point")
+				.remove();
+		},
 	}
 }
 
@@ -157,18 +296,18 @@ export default {
 .wrapper {
 	display: grid;
 	grid-template-columns: repeat(20, 1fr);
-	grid-template-rows: repeat(20, 4.24vh);
+	grid-template-rows: repeat(20, 2.8vh);
 	gap: 10px;
 	height: 100%;
 	width: 100%;
 }
 
 .calendar {
-	grid-area: 1 / 1 / 7 / 5;
+	grid-area: 1 / 1 / 16 / 5;
 }
 
 .map {
-	grid-area: 1 / 5 / 16 / 17;
+	grid-area: 1 / 5 / 39 / 17;
 }
 
 .scatter-plot {
@@ -176,20 +315,20 @@ export default {
 }
 
 .overview {
-	grid-area: 7 / 1 / 21 / 5;
+	grid-area: 16 / 1 / 56 / 5;
 }
 
 .timeline {
-	grid-area: 16 / 5 / 17 / 17;
+	grid-area: 39 / 5 / 41 / 17;
 }
 
 .under-timeline-plot {
-	grid-area: 17 / 5 / 21 / 17;
+	grid-area: 41 / 5 / 56 / 17;
 }
 
 
 .daily-life-plot {
-	grid-area: 9 / 17 / 21 / 21;
+	grid-area: 9 / 17 / 56 / 21;
 }
 
 .request {
@@ -222,198 +361,6 @@ const getData = async (path) => {
 	var res = await HttpHelper.post(Urls.getCSVData, { path: path });
 	console.log(res);
 	return res;
-};
-
-
-var initTransports = function () {
-	d3.csv("../CSVdata/Transports/transports_2023-03-03.csv").then(function (data) {
-		var parseTime = d3.timeParse('%Y-%m-%d %H:%M:%S');
-		var formatTime = d3.timeFormat('%Y-%m-%d %H:%M:%S');
-
-		// Parse the time and calculate average speed
-		data.forEach(function (d) {
-			d.timeStamp = parseTime(d.day + ' ' + d.time);
-			d.timeStampFloat = d.timeStamp.getTime();
-		});
-
-
-		// Create a time scale for the x-axis
-		var timeScale = d3.scaleTime()
-			.domain([0, width])
-			.range(d3.extent(data, function (d) { return d.timeStampFloat; }));
-
-		// Create a color scale for the scatter plot
-		var speedColorScale = d3.scaleLinear()
-			.domain([100, 250, 400])
-			.range(["red", "orange", "green"]);
-
-
-		var timeline = d3.select(".timeline")
-
-		var handle = timeline.select(".handle");
-		var dragBehavior = d3.drag()
-			.on("drag", function dragEvent(event) {
-				clearScatterPlot();
-				deleteSpeedPlot();
-
-				var x = event.x;
-				var max_x = timeline.node().getBoundingClientRect().width - handle.node().getBoundingClientRect().width;
-				if (x < 0) {
-					x = 0;
-				} else if (x > max_x) {
-					x = max_x;
-				}
-				handle.style("left", x + "px");
-				var curTime = x / max_x * (timeScale.range()[1] - timeScale.range()[0]) + timeScale.range()[0];
-
-
-				console.log("curTime: " + formatTime(new Date(curTime)));
-				// console.log("selectData.min_speed: " + d3.min(selectData, function (d) { return d.speed; }));
-				// console.log("selectData.max_speed: " + d3.max(selectData, function (d) { return d.speed; }));
-
-				var selectData = selectDataByCurTime(curTime);
-				drawScatterPlot(selectData);
-
-				var sortedSpeeds = getSpeedBySelectedData(selectData);
-				plotSpeed(sortedSpeeds, 60, 0.75);
-			});
-		handle.call(dragBehavior);
-
-		function selectDataByCurTime(curTime) {
-			var leftMaxTimeStamp = parseFloat(d3.max(data, d => d.timeStampFloat <= curTime ? d.timeStampFloat : -Infinity));
-			var rightMinTimeStamp = parseFloat(d3.min(data, d => d.timeStampFloat >= curTime ? d.timeStampFloat : Infinity));
-			// 边缘情况
-			if (leftMaxTimeStamp == rightMinTimeStamp)
-				if (parseFloat(d3.min(data, d => d.timeStampFloat > curTime ? d.timeStampFloat : Infinity)) == Infinity) {
-					leftMaxTimeStamp = parseFloat(d3.max(data, d => d.timeStampFloat < curTime ? d.timeStampFloat : -Infinity));
-					rightMinTimeStamp = parseFloat(d3.min(data, d => d.timeStampFloat >= curTime ? d.timeStampFloat : Infinity));
-					console.log("case1" + leftMaxTimeStamp + " " + rightMinTimeStamp)
-				} else {
-					leftMaxTimeStamp = parseFloat(d3.max(data, d => d.timeStampFloat <= curTime ? d.timeStampFloat : -Infinity));
-					rightMinTimeStamp = parseFloat(d3.min(data, d => d.timeStampFloat > curTime ? d.timeStampFloat : Infinity));
-				}
-
-			var leftSelectedData = data.filter(d => d.timeStampFloat === leftMaxTimeStamp);
-			var rightSelectedData = data.filter(d => d.timeStampFloat === rightMinTimeStamp);
-			var curRelativeTime = (curTime - leftMaxTimeStamp) / (rightMinTimeStamp - leftMaxTimeStamp);
-
-			var selectData = [];
-			for (let i = 0; i < leftSelectedData.length; i++) {
-				for (let j = 0; j < rightSelectedData.length; j++) {
-					if (leftSelectedData[i].participantId == rightSelectedData[j].participantId) {
-						var tempData = {};
-						tempData.participantId = leftSelectedData[i].participantId;
-						tempData.timeStampFloat = curTime;
-						tempData.timeStamp = formatTime(new Date(curTime));
-						tempData.loc_x = (1 - curRelativeTime) * leftSelectedData[i].loc_x + curRelativeTime * rightSelectedData[j].loc_x;
-						tempData.loc_y = (1 - curRelativeTime) * leftSelectedData[i].loc_y + curRelativeTime * rightSelectedData[j].loc_y;
-						tempData.speed = Math.sqrt((rightSelectedData[j].loc_x - leftSelectedData[i].loc_x) ** 2 + (rightSelectedData[j].loc_y - leftSelectedData[i].loc_y) ** 2);
-						selectData.push(tempData);
-						break;
-					}
-				}
-			}
-			return selectData;
-		}
-
-		function selectDataById(id) {
-			return data.filter(d => d.participantId === id);
-		}
-
-		function getSpeedBySelectedData(selectData) {
-			var speeds = [];
-			for (let i = 0; i < selectData.length; i++) {
-				speeds.push(selectData[i].speed);
-			}
-			var sortedSpeeds = speeds.sort(function (a, b) { return a - b; });
-			return sortedSpeeds;
-		}
-
-		function plotSpeed(sortedSpeeds, Y_max, gamma) {
-			// Calculate the count for each range
-			var countData = [];
-			for (var i = 0; i <= 500; i += 50) {
-				var count = sortedSpeeds.filter(function (d) {
-					return i != 500 ? d >= i && d < i + 50 : d >= i;
-				}).length;
-				countData.push({ range: i, count: count });
-			}
-
-			// Create scales for x and y axes
-			var xScale = d3.scaleBand()
-				.domain(countData.map(function (d) { return d.range; }))
-				.range([0, width])
-				.padding(0.1);
-
-			var yScale = d3.scaleLinear()
-				.domain([0, Y_max])
-				.range([height, 0]);
-
-			// Create line generator
-			var line = d3.line()
-				.x(function (d) { return xScale(d.range) + xScale.bandwidth() / 2; })
-				.y(function (d) { return yScale(d.count ** gamma); });
-
-			// Append the line chart
-			svg.append("path")
-				.datum(countData)
-				.attr("class", "line-chart")
-				.attr("d", line)
-				.attr("fill", "none")
-				.attr("stroke", "blue")
-				.attr("stroke-width", 2);
-
-			// Append the x axis
-			svg.append("g")
-				.attr("transform", "translate(0," + height + ")")
-				.call(d3.axisBottom(xScale));
-
-			// Append the y axis
-			svg.append("g")
-				.call(d3.axisLeft(yScale));
-		}
-
-		function deleteSpeedPlot() {
-			svg.selectAll(".line-chart").remove();
-			svg.selectAll("g").remove();
-		}
-
-		function drawScatterPlot(selectData) {
-			// Draw the scatter plot
-			svg.selectAll("circle")
-				.data(selectData)
-				.enter()
-				.append("circle")
-				.attr("class", "scatter-point")
-				.attr("cx", function (d) { return xScale(d.loc_x); })
-				.attr("cy", function (d) { return yScale(d.loc_y); })
-				.attr("r", 2)
-				.attr("fill-opacity", 1.0)
-				.attr("fill", function (d) { return speedColorScale(d.speed); })
-				// TODO
-				.on("mouseover", function (d) {
-					var newSelectedData = data.filter(d => d.participantId === d.participantId);
-					d3.select(this)
-						.attr("fill", "yellow")
-						.attr("stroke-width", 1.2);
-				})
-				.on("mouseout", function (d) {
-					console.log("mouseout " + d.participantId);
-					d3.selectAll("circle.scatter-point.id")
-						.attr("fill", function (d) {
-							return speedColorScale(d.speed);
-						})
-						.attr("stroke-width", 0.8);
-				});
-
-		};
-
-		function clearScatterPlot() {
-			// Update the scatter plot
-			svg.selectAll("circle.scatter-point")
-				.remove();
-		};
-	});
 };
 
 </script> -->
