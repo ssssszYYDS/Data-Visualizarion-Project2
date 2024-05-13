b1887f2feba6cbad1dd77d5810f4e99f53b407f4
ssssszYYDS <xiakj@shanghaitech.edu.cn>
2024-05-13 17:33:43 +0800

    name-update

--- packages/client/src/components/Calendar.vue
+++ packages/client/src/components/Calendar.vue
@@ -2,10 +2,10 @@
 
 <template>
     <button @click="setDate1" style="width: 100%; height: 30%;">
-        setDate to '2022-03-01'
+        set date to '2022-03-01'
     </button>
     <button @click="setDate2" style="width: 100%; height: 30%;">
-        setDate to '2022-03-05'
+        set date to '2022-03-05'
     </button>
 </template>
 
@@ -23,12 +23,12 @@ export default {
     methods: {
         setDate1() {
             console.log('set date to 2023-03-01');
-            this.$emit('update:date', '2022-03-01'); // 触发 update:date 事件，并传递新的日期值
+            this.$emit('update:date', '2022-03-01');
         },
 
         setDate2() {
             console.log('set date to 2023-03-05');
-            this.$emit('update:date', '2022-03-05'); // 触发 update:date 事件，并传递新的日期值
+            this.$emit('update:date', '2022-03-05');
         },
     }
 }
